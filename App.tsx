cc5dc4b6ab961b9e3fdfe198e576b2eefcd98f57
Daniel Torres <daniel.cadarn@gmail.com>
2024-05-04 11:27:45 -0300

    Aprimorando layout

--- App.tsx
+++ App.tsx
@@ -6,15 +6,16 @@ export default function App() {
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
         <View>
+          <Text style={styles.title}>KEY GENERATOR</Text>
           <Image
             source={require("./assets/logo-app.png")}
             style={styles.image}
           />
         </View>
         <View>
-          <Text>TEXTO AQUI</Text>
+          <Text style={styles.keyText}>A CHAVE APARECE AQUI!</Text>
           <Button title="Gerar chave aleatória" />
-          <Button title="Copiar" />
+          <Button title="Copiar Chave" />
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
@@ -24,14 +25,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#0f0f0f",
     alignItems: "center",
     justifyContent: "center",
+    fontFamily: "Roboto",
   },
   image: {
     height: 250,
     width: 250,
-    marginTop: -250,
     marginBottom: 18,
+  },
+  keyText: {
+    textAlign: "center",
+    marginBottom: 50,
+    fontSize: 18,
+    color: "#fff",
+    backgroundColor: "pink",
+    borderRadius: 10,
+    padding: 5,
+  },
+  title: {
+    fontSize: 24,
+    textAlign: "center",
+    fontWeight: "800",
+
+    color: "#fff",
   },
 });
