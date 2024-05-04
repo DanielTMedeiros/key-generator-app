6843a50c1fcae12b9a6345cf2dac1029138e6d17
Daniel Torres <daniel.cadarn@gmail.com>
2024-05-04 13:06:41 -0300

    Logica da geracao de chave finalizada

--- App.tsx
+++ App.tsx
@@ -3,18 +3,32 @@ import { StyleSheet, Text, View, Image, Button } from "react-native";
 import { useState } from "react";
 
 export default function App() {
-  const [key, SetKey] = useState("Estado Inicial!");
+  const [key, setKey] = useState("Estado Inicial!");
+  const [style, setStyle] = useState(styles.keyText);
 
   const generateKey = () => {
+    const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const specialChars = "!@#$%&*";
     const characters =
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*";
-    let result = "";
+
+    let result = [];
+
+    result.push(
+      upperCaseChars.charAt(Math.floor(Math.random() * upperCaseChars.length))
+    );
+    result.push(
+      specialChars.charAt(Math.floor(Math.random() * specialChars.length))
+    );
     for (let i = 0; i < 10; i++) {
-      result += characters.charAt(
-        Math.floor(Math.random() * characters.length)
+      result.push(
+        characters.charAt(Math.floor(Math.random() * characters.length))
       );
     }
-    return SetKey(result);
+    let finalResult = result.sort(() => Math.random() - 0.5).join("");
+
+    setKey(finalResult.toString());
+    setStyle(styles.keyTextPressed);
   };
 
   return (
@@ -23,12 +37,12 @@ export default function App() {
         <View>
           <Text style={styles.title}>KEY GENERATOR</Text>
           <Image
-            source={require("./assets/logo-app.png")}
+            source={require("./assets/logo-app-new.png")}
             style={styles.image}
           />
         </View>
         <View>
-          <Text style={styles.keyText}>{key}</Text>
+          <Text style={style}>{key}</Text>
           <Button title="Gerar chave aleatória" onPress={generateKey} />
           <Button title="Copiar Chave" />
         </View>
@@ -58,6 +72,18 @@ const styles = StyleSheet.create({
     color: "#0f0f0f",
     fontWeight: "600",
     backgroundColor: "#FAE516",
+    borderRadius: 10,
+    padding: 12,
+  },
+
+  keyTextPressed: {
+    textAlign: "center",
+    textAlignVertical: "center",
+    marginBottom: 50,
+    fontSize: 20,
+    color: "#0f0f0f",
+    fontWeight: "600",
+    backgroundColor: "#4AF947",
     borderRadius: 10,
     padding: 12,
   },
